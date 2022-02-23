1912dd83d6b8f9269756b0eb9fd74df792e35fda
Christopher Jung <chris@pop-os.localdomain>
2022-02-23 12:57:34 +0100

    self-update feature

---
+++ update.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+COMPOSE_FILE_NAME="docker-compose.yml"
+
+function updateAndStart() {
+	local container_name=$1
+	local folder=$2
+	local needUpdate=`checkForUpdate $container_name $folder` # grab the stdout from checkForUpdate
+	echo "needUpdate=$needUpdate"
+	if [ $needUpdate == "true" ]; then rebuildImage "$folder"; fi
+	startService $folder
+}
+
+function checkForUpdate() {
+	local container_name=$1
+	local folder=$2
+	local usedVersion=`docker inspect --format '{{ index .Config.Labels "version"}}' $container_name`
+	local currentVersion=`grep "LABEL version" "$folder/Dockerfile" | awk -F= '{print $2}'`
+	if [ "$currentVersion" != "$usedVersion" ]; then
+		echo "true"
+	else
+		echo "false"
+	fi
+}
+
+function rebuildImage() {
+	local folder=$1
+	cd $folder
+	docker-compose -f $COMPOSE_FILE_NAME down --remove-orphans
+	docker image prune -af
+	docker-compose -f $COMPOSE_FILE_NAME build
+	cd ..
+}
+
+function startService() {
+	local folder=$1
+	cd $folder
+	docker-compose -f $COMPOSE_FILE_NAME up -d
+	cd ..
+}
+
+function startServer() {
+	echo `date`
+	git fetch
+	git reset --hard origin/master
+	updateAndStart "object_detection_analyzer" "object_detection"
+	startService "motioneye"
+}
+
+startServer > update.log
