97de8f291fc45e6c59eb6857c46af9ca48dd5156
Christopher jung <mail@jung-christopher.de>
2022-02-25 18:14:25 +0100

    Add crontab auto-update

--- update.sh
+++ update.sh
@@ -48,4 +48,19 @@ function startServer() {
 	startService "motioneye"
 }
 
-startServer > /media/usbstick/update.log
+function updateCrontab() {
+	usedCrontab=`sed '/#BEECAM-START/,/#BEECAM-END/!d;//d' /etc/crontab | tr -d '\n' | sed -E "s/,\s*/\n/g"`
+	currentCrontab=$(cat crontab.txt | sed -e 's/\//\\\//g')
+	if [ "$currentCrontab" != "$usedCrontab" ]; then
+		echo "Updating crontab!"
+		perl -i -pe "BEGIN{undef $/;} s/#BEECAM-START.*#BEECAM-END/#BEECAM-START\n$currentCrontab\n#BEECAM-END/smg" /etc/crontab
+		/etc/init.d/cron reload
+	fi
+}
+
+function update() {
+	sudo -u pi startServer
+	updateCrontab
+}
+
+update > /media/usbstick/update.log
