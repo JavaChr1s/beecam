9e4442ed3ef2dee2a14056d2953b23f5923233fd
Christopher Jung <chris@pop-os.localdomain>
2022-04-02 15:46:49 +0200

    Call critical scripts async

--- webhook/hooks/index.sh
+++ webhook/hooks/index.sh
@@ -18,16 +18,19 @@ cat << EOF
             }
         </style>
         <script type="text/javascript">
-            function refreshDate() {
-                var date = new Date();
+            function callAsync(url) {
                 var req = new XMLHttpRequest();
                 req.onreadystatechange=function() {
                     if (req.readyState==4 && req.status==200) {
                         window.location.reload();
                     }
                 }
-                req.open("GET", "/set-date?date=" + "" + ('00'+(date.getMonth() + 1)).slice(-2) + ('00'+date.getDate()).slice(-2) + ('00'+date.getHours()).slice(-2) + ('00'+date.getMinutes()).slice(-2) + date.getFullYear());
+                req.open("GET", url);
                 req.send();
+            }
+            function refreshDate() {
+                var date = new Date();
+                callAsync("/set-date?date=" + "" + ('00'+(date.getMonth() + 1)).slice(-2) + ('00'+date.getDate()).slice(-2) + ('00'+date.getHours()).slice(-2) + ('00'+date.getMinutes()).slice(-2) + date.getFullYear());
             }
         </script>
 	</head>
@@ -44,14 +47,14 @@ cat << EOF
         <ul>
             <li><a href="#motioneye" onClick="window.location = new window.URL(window.location.href).origin + ':8765'">MotionEye</a></li>
             <li><a href="/list-data">Print USB-Data</a></li>
-            <li><a href="/reboot" onclick="return confirm('Are you sure you want to reboot the beecam?')">Reboot</a></li>
-            <li><a href="/shutdown" onclick="return confirm('Are you sure you want to shutdown the beecam?')">Shutdown</a></li>
+            <li><a href="/reboot" onclick="if(confirm('Are you sure you want to reboot the beecam?')) callAsync('/reboot'); return false;">Reboot</a></li>
+            <li><a href="/shutdown" onclick="if(confirm('Are you sure you want to shutdown the beecam?')) callAsync('/shutdown'); return false;">Shutdown</a></li>
             <li><a href="/save-config">Save MotionEye configurations (available only if online)</a></li>
         </ul>
         <h2>Technical Links</h2>
         <ul>
             <li><a href="/status">Status</a></li>
-            <li><a href="/update" onclick="return confirm('Are you sure you want to update the beecam?')">Update</a></li>
+            <li><a href="/update" onclick="if(confirm('Are you sure you want to update the beecam?')) callAsync('/update'); return false;">Update</a></li>
             <li><a href="/logs-analyzer">Logs: Analyzer</a></li>
             <li><a href="/logs-motioneye">Logs: MotionEye</a></li>
             <li><a href="/logs-webhook">Logs: Webhook</a></li>
