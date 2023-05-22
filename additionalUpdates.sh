b013d144ddfaef758470e20653a9dac21eaf7658
Christopher Jung <office@jung-christopher>
2023-05-22 09:24:50 +0200

    Install maxcpus param automatically

--- additionalUpdates.sh
+++ additionalUpdates.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# install maxcpus param
+if grep -q "maxcpus=" /boot/cmdline.txt
+then
+    echo "maxcpus param is already installed"
+else
+    mount -o remount,rw /boot
+    sed -i 's/console=tty1 /console=tty1 maxcpus=4 /g' /boot/cmdline.txt
+    mount -o remount,ro /boot
+fi
+
 # PERFORMANCE
 
 # Disable ethernet
